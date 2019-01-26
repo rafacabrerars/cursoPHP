59e948d7810b08296becb03199bf0bed3ad6314b
Juciellen Cabrera <jucarol17@gmail.com>
2019-01-26 09:28:49 -0200

    alteração arquivo

--- parouimpar.php
+++ parouimpar.php
@@ -7,9 +7,10 @@
 <?php
 $operacao = null;
 $num1 = ($_POST['num1']) ?? null;
-$num2 = "2" ?? null;
+$num2 = 2;
 $operacao = $num1 % $num2;
-if($operacao == '0'){
+
+if($operacao == 0){
     echo "Numero PAR";
 }else
     echo "Numero IMPAR";
