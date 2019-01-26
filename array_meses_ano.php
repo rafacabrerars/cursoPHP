7260a021855a5e779ded79b7169af934ea574051
Juciellen Cabrera <jucarol17@gmail.com>
2019-01-26 13:00:33 -0200

    adicionando conteudo novo

--- array_meses_ano.php
+++ array_meses_ano.php
@@ -1,2 +1,23 @@
 <?php
 echo '<pre>';
+
+$mes=[
+    [
+        'id'=>1, 'nome'=>'Janeiro'], [
+        'id'=>2, 'nome'=>'Fevereiro'], [
+        'id'=>3, 'nome'=>'Março'], [
+        'id'=>4, 'nome'=>'Abril'], [
+        'id'=>5, 'nome'=>'Maio'], [
+        'id'=>6, 'nome'=>'Junho'], [
+        'id'=>7, 'nome'=>'Julho'], [
+        'id'=>8, 'nome'=>'Agosto'], [
+        'id'=>9, 'nome'=>'Setembro'], [
+        'id'=>10, 'nome'=>'Outubro'], [
+        'id'=>11, 'nome'=>'Novembro'], [
+        'id'=>12, 'nome'=>'Dezembro'],];
+/*echo '<pre>';
+print_r($mes);*/
+
+foreach ($mes as $meses) {
+    echo "<h5>{$meses ['id']} - {$meses ['nome']}";
+}
--- array_multidimensional.php
+++ array_multidimensional.php
@@ -1,37 +1,24 @@
 <?php
 $alunos = [
     [
-        'id'=> 1,
-        'nome' => 'Ricardo'
-    ],
-    [
-        'id'=> 2,
-        'nome' => 'Wesley'
-    ],
-    [
-        'id'=> 3,
-        'nome' => 'Felipe'
-    ],
-    [
-            'id'=> 4,
-            'nome' => 'Rafael'
-    ],
-    [
-        'id'=> 5,
-        'nome' => 'Camila'
-    ],
-    [
-        'id'=> 6,
-        'nome' => 'Sidclei'
-    ],
-    [
-        'id'=> 7,
-        'nome' => 'Alexandre'
-    ],
+        'id' => 1 , 'nome' => 'Ricardo'
+    ] , [
+        'id' => 2 , 'nome' => 'Wesley'
+    ] , [
+        'id' => 3 , 'nome' => 'Felipe'
+    ] , [
+        'id' => 4 , 'nome' => 'Rafael'
+    ] , [
+        'id' => 5 , 'nome' => 'Camila'
+    ] , [
+        'id' => 6 , 'nome' => 'Sidclei'
+    ] , [
+        'id' => 7 , 'nome' => 'Alexandre'
+    ] ,
 ];
 echo '<pre>';
 print_r($alunos);
 
-foreach ($alunos as $aluno){
+foreach ($alunos as $aluno) {
     echo "<h5>{$aluno ['id']} - {$aluno ['nome']}";
 }
