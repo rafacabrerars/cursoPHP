d6179cc5d147547b50d34ad4411a6dcbc2cc812e
Juciellen Cabrera <jucarol17@gmail.com>
2019-02-02 17:50:39 -0200

    update de conteudo

--- formulario_php.php
+++ formulario_php.php
@@ -1,7 +1,31 @@
 <?php
-print_r($_POST);
+//print_r($_POST);
 $nome = $_POST['nome'];
 $email = $_POST['email'];
 $senha = $_POST['senha'];
 $senha_confirmacao = $_POST['senha_confirmacao'];
 $foto = $_FILES['foto'];
+
+if (empty($nome)){
+echo 'vc esqueceu seu nome?';
+}
+echo "<pre>";
+
+if (empty($email)){
+    echo 'serio que voce nao tem email ?';
+}
+echo "<pre>";
+
+if (empty($senha)){
+    echo 'A SENHA .... pô';
+}
+echo "<pre>";
+
+if (empty($senha_confirmacao)){
+    echo 'confima a senha panois';
+}
+echo "<pre>";
+
+if (empty($foto)){
+    echo 'faltou a fotinha';
+}
