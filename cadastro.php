4a99e35453b53148709ff765126642fc7fb67f1e
Juciellen Cabrera <jucarol17@gmail.com>
2019-02-02 17:07:03 -0200

    update de conteudo

--- cadastro.php
+++ cadastro.php
@@ -2,7 +2,7 @@
     Nome :<input type="text" name="nome"><br>
     E-mail :<input type="text" name="email"><br>
     Senha :<input type="password" name="senha"><br>
-    Confirmar senha :<input type="password" name="senha-confirmacao"><br>
+    Confirmar senha :<input type="password" name="senha_confirmacao"><br>
     Foto :<input type="file" name="foto">
     <input type="submit" value="calcular">
 </form>
--- formulario_php.php
+++ formulario_php.php
@@ -1,3 +1,7 @@
 <?php
 print_r($_POST);
 $nome = $_POST['nome'];
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+$senha_confirmacao = $_POST['senha_confirmacao'];
+$foto = $_FILES['foto'];
